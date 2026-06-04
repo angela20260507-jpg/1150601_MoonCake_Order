d9624d2332699478703ec782250b7ade85fba270
周倩夙 <angela20260507@gmail.com>
2026-06-04 21:53:26 +0800

    修改5

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/1150601_MoonCake_Order/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
